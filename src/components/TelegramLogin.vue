63dd199ee7b54552ec89f0124d73652ba43b8a0e
u7NGAQhsej <aleksandr.ostromogilskii@gmail.com>
2022-11-05 01:25:02 +0300

    create telegram widget component

---
+++ src/components/TelegramLogin.vue
@@ -0,0 +1,32 @@
+<template>
+  <div ref="telegramWrapper"></div>
+</template>
+
+<script>
+
+export default {
+  name: 'TelegramLogin',
+  mounted(){
+    this.init()
+  },
+  methods:{
+    onTelegramAuth(user){
+      alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
+    },
+    init(){
+      window.onTelegramAuth = this.onTelegramAuth
+      const script = document.createElement('script')
+      script.setAttribute("src", "https://telegram.org/js/telegram-widget.js?21")
+      script.setAttribute("data-telegram-login", "CalendarEventerBot")
+      script.setAttribute("data-size", "large")
+      script.setAttribute("data-onauth", "onTelegramAuth(user)")
+      script.setAttribute("data-request-access", "write")
+      script.setAttribute("async", "true")
+      this.$refs.telegramWrapper.appendChild(script)
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <telegram-login/>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import TelegramLogin from '@/components/TelegramLogin.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    TelegramLogin
   }
 }
 </script>
