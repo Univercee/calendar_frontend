55f87420a7550030443a8c1f9850205667af2d5c
u7NGAQhsej <aleksandr.ostromogilskii@gmail.com>
2022-11-05 01:23:44 +0300

    update local dev server

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    host: process.env.BASE_URL ?? "127.0.0.1",
+    port: process.env.PORT ?? 80,
+  }
 })
