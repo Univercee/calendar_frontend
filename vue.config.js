5092150c81c61d63b81270f242a577240fc18a7c
u7NGAQhsej <aleksandr.ostromogilskii@gmail.com>
2022-11-05 17:38:18 +0300

    install axios

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    host: process.env.BASE_URL ?? "127.0.0.1",
-    port: process.env.PORT ?? 80,
+    host: process.env.VUE_APP_BASE_URL ?? "127.0.0.1",
+    port: process.env.VUE_APP_PORT ?? 80,
+    proxy: process.env.VUE_APP_SERVER_URL
   }
 })
