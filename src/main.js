750bba12592bcf1e703428438d27c2a46805b842
u7NGAQhsej <aleksandr.ostromogilskii@gmail.com>
2022-11-08 00:31:36 +0300

    install bootstrap and calendar

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VCalendar from 'v-calendar';
 import './axios'
 
-createApp(App).use(store).use(router).mount('#app')
+import 'v-calendar/dist/style.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+
+createApp(App).use(store).use(router).use(VCalendar, {}).mount('#app')
