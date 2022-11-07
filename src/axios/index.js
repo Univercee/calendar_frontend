d93faf6a791be7ae8bd90d3dc96e5ccb5d78e56c
u7NGAQhsej <aleksandr.ostromogilskii@gmail.com>
2022-11-08 00:32:21 +0300

    refactoring

--- src/axios/index.js
+++ src/axios/index.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 window.axios = axios;
 axios.defaults.baseURL = process.env.VUE_APP_SERVER_URL;
 axios.defaults.headers.common['Content-Type'] = 'application/json';
-let sessionKey = sessionStorage.getItem('sessionKey');
-if(sessionKey) axios.defaults.headers.common.Authorization = 'Bearer ' + sessionKey;
+let token = sessionStorage.getItem('token');
+if(token) axios.defaults.headers.common.Authorization = 'Bearer ' + token;
