728ebaa47d60b6f1be332a1fbd9b4455b20b198c
u7NGAQhsej <aleksandr.ostromogilskii@gmail.com>
2022-11-09 16:04:18 +0300

    fonst and styles

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,14 @@
 </template>
 
 <style>
-
+@font-face {
+  font-family: "Iceland";
+  src: local("Iceland"),
+  url(@/assets/fonst/Iceland-Regular.ttf);
+}
+@font-face {
+  font-family: "Roboto";
+  src: local("Roboto"),
+  url(@/assets/fonst/Roboto-Regular.ttf);
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import VCalendar from 'v-calendar';
 import './axios'
 
 import 'v-calendar/dist/style.css';
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
 
 createApp(App).use(store).use(router).use(VCalendar, {}).mount('#app')
